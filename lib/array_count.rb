cd57cfcd75bf1e3815bb02d5a43819e84403ab3e
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-28 19:47:45 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,14 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   counter = 0;
+  strings = [];
   while counter < array.length do
     array[counter].class == String
+    strings.push(array[counter])
+  end
+  counter += 1;
+end
+return strings.count
 end
 
 def count_empty_strings(array)
